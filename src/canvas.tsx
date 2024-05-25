6df6f662ad2ac244d20a8193e4134cfabe70cb48
bigmistqke <vandijckv@gmail.com>
2024-05-25 18:24:46 +0200

    wrap initialisation in onMount
    before
    - createThree and other initialisation code was called immediately after declaring canvas
    - this caused issues with binding pointer-events on macOS chrome 125.0.6422.78 (arm64)
    
    after
    - createThree and other initialisation code is wrapped in onMount

--- src/canvas.tsx
+++ src/canvas.tsx
@@ -1,5 +1,5 @@
 import { createResizeObserver } from "@solid-primitives/resize-observer";
-import { ComponentProps, JSX, createRenderEffect, splitProps } from "solid-js";
+import { ComponentProps, JSX, createRenderEffect, onMount, splitProps } from "solid-js";
 import {
   Camera,
   OrthographicCamera,
@@ -92,32 +92,34 @@ export function Canvas(_props: CanvasProps) {
     <div style={{ width: "100%", height: "100%" }}>{canvas}</div>
   ) as HTMLDivElement;
 
-  const context = createThree(canvas, props);
+  onMount(() => {
+    const context = createThree(canvas, props);
 
-  // Resize observer for the canvas to adjust camera and renderer on size change
-  createResizeObserver(
-    () => container,
-    () => {
-      context.gl.setSize(window.innerWidth, window.innerHeight);
-      context.gl.setPixelRatio(window.devicePixelRatio);
+    // Resize observer for the canvas to adjust camera and renderer on size change
+    createResizeObserver(
+      () => container,
+      () => {
+        context.gl.setSize(window.innerWidth, window.innerHeight);
+        context.gl.setPixelRatio(window.devicePixelRatio);
 
-      if (context.camera instanceof OrthographicCamera) {
-        context.camera.left = window.innerWidth / -2;
-        context.camera.right = window.innerWidth / 2;
-        context.camera.top = window.innerHeight / 2;
-        context.camera.bottom = window.innerHeight / -2;
-      } else {
-        context.camera.aspect = window.innerWidth / window.innerHeight;
-      }
-      context.camera.updateProjectionMatrix();
-      context.render(performance.now());
-    },
-  );
+        if (context.camera instanceof OrthographicCamera) {
+          context.camera.left = window.innerWidth / -2;
+          context.camera.right = window.innerWidth / 2;
+          context.camera.top = window.innerHeight / 2;
+          context.camera.bottom = window.innerHeight / -2;
+        } else {
+          context.camera.aspect = window.innerWidth / window.innerHeight;
+        }
+        context.camera.updateProjectionMatrix();
+        context.render(performance.now());
+      },
+    );
 
-  // Assign ref
-  createRenderEffect(() => {
-    if (props.ref instanceof Function) props.ref(container);
-    else props.ref = container;
+    // Assign ref
+    createRenderEffect(() => {
+      if (props.ref instanceof Function) props.ref(container);
+      else props.ref = container;
+    });
   });
 
   return (
